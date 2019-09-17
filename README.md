1eaa2d69411dd89c07640724d2c70520572a403f
Adrian Wilke <adibaba@users.noreply.github.com>
2019-09-17 15:26:19 +0200

    created templates

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Setup file(s) for OPAL data server with one Elasticsearch and one Apache Fuseki instance.
 
-Some documentatin is in the wiki: https://github.com/projekt-opal/opaldata/wiki
+Some documentation is in the GitHub wiki: https://github.com/projekt-opal/opaldata/wiki
---
+++ archive/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Please provide an admin password for Fuseki"
+    exit 1
+fi
+FUSEKI_ADMIN_PASSWORD=$1
+
+docker run --name fuseki-data -v /fuseki busybox
+docker run -d -p 3030:3030 -it -e ADMIN_PASSWORD=${FUSEKI_ADMIN_PASSWORD} --name=fuseki --volumes-from fuseki-data  stain/jena-fuseki
+docker network create opalnetwork
+docker run -d --name es -p 9200:9200 -p 9300:9300 --network opalnetwork -e "discovery.type=singel-node" elasticsearch:7.3.1
+docker run -d --name kibana --network opalnetwork -e "ELASTICSEARCH_URL=http://es:9200" -p 5601:5601 kibana:7.3.1
